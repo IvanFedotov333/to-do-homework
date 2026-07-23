a250614242867de3ee50611d2519284dfa191f9c
IvanFedotov333 <ivan_fedotov_2013@inbox.ru>
2026-07-23 20:36:39 +0300

    ДЗ №13

--- js/script.js
+++ js/script.js
@@ -3,21 +3,25 @@ const headerInput = document.querySelector(".header-input");
 const todoList = document.querySelector(".todo-list");
 const todoCompleted = document.querySelector(".todo-completed");
 
-const toDoData = [];
+let todoData = [];
+
+const setToLocalStorage = function () {
+  localStorage.setItem("todoData", JSON.stringify(todoData));
+};
 
 const render = function () {
   todoList.innerHTML = "";
   todoCompleted.innerHTML = "";
-  toDoData.forEach(function (item) {
+  todoData.forEach(function (item, index) {
     const li = document.createElement("li");
     li.classList.add("todo-item");
-    li.innerHTML =
-      '<span class="text-todo">' +
-      item.text +
-      '<div class="todo-buttons">' +
-      '<button class="todo-remove"></button>' +
-      '<button class="todo-complete"></button>' +
-      "</div>";
+    li.innerHTML = `
+      <span class="text-todo">${item.text}</span>
+      <div class="todo-buttons">
+        <button class="todo-remove"></button>
+        <button class="todo-complete"></button>
+      </div>
+    `;
     if (item.completed) {
       todoCompleted.append(li);
     } else {
@@ -25,21 +29,37 @@ const render = function () {
     }
     li.querySelector(".todo-complete").addEventListener("click", function () {
       item.completed = !item.completed;
+      setToLocalStorage();
+      render();
+    });
+    li.querySelector(".todo-remove").addEventListener("click", function () {
+      todoData.splice(index, 1);
+      setToLocalStorage();
       render();
     });
   });
 };
 
+const localStoredData = localStorage.getItem("todoData");
+
+if (localStoredData) {
+  todoData = JSON.parse(localStoredData);
+}
+render();
+
 todoControl.addEventListener("submit", function (event) {
   event.preventDefault();
+  if (headerInput.value.trim() === "") {
+    return;
+  }
 
   const newTodo = {
     text: headerInput.value,
     completed: false,
   };
 
-  toDoData.push(newTodo);
+  todoData.push(newTodo);
   headerInput.value = "";
-
+  setToLocalStorage();
   render();
 });
